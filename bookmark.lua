72ab6ca31711af35518f55559bcf3fe897793801
wowmao <72061128+wowmao@users.noreply.github.com>
2023-03-12 13:34:50 +0800

    add bookmark to playlist script

---
+++ bookmark.lua
@@ -0,0 +1,13 @@
+function print_filename()
+  local filepath = mp.get_property("path")
+  local filename = mp.get_property("filename")
+  local log = "Added to bookmark: " .. filepath
+  print(log)
+  local output = filepath
+  local f = io.open(os.getenv("HOME") .. "/mpv_bookmark.txt", "a")
+  f:write(output .. "\n")
+  f:close()
+end
+
+mp.add_key_binding("ctrl+s", "print_filename", print_filename)
+
